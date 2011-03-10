dc1e2db6a3dbe0e86d397ce6b1aed103b74c94e8
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-09 22:15:51 -0500

    Add the rest of my dotfiles

--- link.rb
+++ link.rb
@@ -1,7 +1,7 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
-dot_files = %w{js terminitor}
+dot_files = %w{js terminitor ackrc bash_login gitconfig gitignore irbrc}
 
-dot_files.each do |dir|
-  `ln -nfs #{current_directory}/#{dir} ~/.#{dir}`
+dot_files.each do |dot|
+  `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
 end
