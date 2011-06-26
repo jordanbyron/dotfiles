454ff6d9160500608d022ebc39a886bdcac36462
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-26 14:43:13 -0400

    Add NHS terminitor and add test tabs for UW and PN

--- link.rb
+++ link.rb
@@ -1,7 +1,11 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
-dot_files = %w{js terminitor ackrc bash_login gitconfig gitignore irbrc}
+dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
 end
+
+# Terminitor is special
+
+`ln -nfs #{current_directory}/terminitor ~/.config/terminitor`
