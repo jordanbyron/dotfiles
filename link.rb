b6df8a2b36926904cbd8f028f23744767605de5e
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-31 11:32:16 -0500

    Add Simple TODOs
    http://blog.jerodsanto.net/2010/12/minimally-awesome-todos/

--- link.rb
+++ link.rb
@@ -1,6 +1,6 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
-dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc}
+dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
