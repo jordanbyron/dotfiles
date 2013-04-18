592623e81630a6231e0b01ecea9d875196afbf47
Jordan Byron <jordan.byron@gmail.com>
2013-04-18 17:08:48 -0400

    vim

--- link.rb
+++ link.rb
@@ -1,12 +1,7 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
-dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo}
+dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo vim vimrc}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
 end
-
-# Terminitor is special
-
-# `ln -nfs #{current_directory}/terminitor ~/.config/terminitor`
-
