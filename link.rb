a0edfe41a38e1e0d1dd0cf9d0b2d7e1014345609
Jordan Byron <jordan.byron@gmail.com>
2013-04-21 11:59:46 -0400

    Add freetds.conf

--- link.rb
+++ link.rb
@@ -1,7 +1,7 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
 dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo vim vimrc
-               gemrc inputrc synergy}
+               gemrc inputrc synergy freetds.conf}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
