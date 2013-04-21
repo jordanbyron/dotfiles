8f5e8de9cb8ab7d35c0c8038b3c2797ab8cc9ae7
Jordan Byron <jordan.byron@gmail.com>
2013-04-21 11:55:11 -0400

    Add synergy config

--- link.rb
+++ link.rb
@@ -1,7 +1,7 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
 dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo vim vimrc
-               gemrc inputrc}
+               gemrc inputrc synergy}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
