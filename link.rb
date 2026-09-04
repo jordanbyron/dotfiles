14afd403eec77dfd220f7f135adc0116e16bf036
Jordan Byron <jordan@jordanbyron.com>
2026-09-03 21:31:50 -0400

    Sync nvim config, link it, and track zshrc (#2)

--- link.rb
+++ link.rb
@@ -1,8 +1,11 @@
 current_directory = File.dirname(File.expand_path(__FILE__))
 
-dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo vim vimrc
-               gemrc inputrc synergy freetds.conf git-worktree.zsh}
+dot_files = %w{js ackrc bash_login gitconfig gitignore irbrc todo gemrc
+               inputrc synergy freetds.conf git-worktree.zsh zshrc}
 
 dot_files.each do |dot|
   `ln -nfs #{current_directory}/#{dot} ~/.#{dot}`
 end
+
+`mkdir -p ~/.config/nvim`
+`ln -nfs #{current_directory}/nvim/init.vim ~/.config/nvim/init.vim`
